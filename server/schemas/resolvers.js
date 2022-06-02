ea273c6f2c5196abbba563777049c25c8dbb85af
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-06-02 11:13:33 -0500

    hooked up the graphQLski

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,120 +1,37 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+
+const { Matchup, Tech } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate('thoughts');
+    matches: async () => {
+      // Get and return all documents from the matches collection
+      return await Matchup.find({});
     },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
+    match: async (parent, args) => {
+      // Use the parameter to find the matching match in the collection
+      return await Matchup.findById(args.id);
     },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
-    },
-    me: async (parent, args, context) => {
-      if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('thoughts');
-      }
-      throw new AuthenticationError('You need to be logged in!');
+    allTech:  async () => {
+      // Get and return all documents from the matches collection
+      return await Tech.find({});
     },
   },
-
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('No user found with this email address');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    addThought: async (parent, { thoughtText }, context) => {
-      if (context.user) {
-        const thought = await Thought.create({
-          thoughtText,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    addComment: async (parent, { thoughtId, commentText }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $addToSet: {
-              comments: { commentText, commentAuthor: context.user.username },
-            },
-          },
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    removeThought: async (parent, { thoughtId }, context) => {
-      if (context.user) {
-        const thought = await Thought.findOneAndDelete({
-          _id: thoughtId,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    removeComment: async (parent, { thoughtId, commentId }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $pull: {
-              comments: {
-                _id: commentId,
-                commentAuthor: context.user.username,
-              },
-            },
-          },
-          { new: true }
-        );
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-  },
+    addMatchup: async (parent, {tech1, tech2}) => {
+          const matchup = await Matchup.create({tech1, tech2});
+          return matchup;
+        },
+    updateMatchupVote: async (parent, { id, whichTech }) => {
+      // Find and update the matching class using the destructured args
+      return await Matchup.findOneAndUpdate(
+        { _id: id }, 
+        { $inc: { [`tech${whichTech}_votes`]: 1 } },
+        // Return the newly updated object instead of the original
+        { new: true }
+      );
+    }
+  }
 };
-
+  
 module.exports = resolvers;
